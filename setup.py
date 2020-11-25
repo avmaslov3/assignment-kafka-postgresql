1937b4818600a0273b099befb8f36acd60816e47
al31415 <maslov@fastmail.nl>
2020-11-25 11:49:04 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 install_requires = [
-      "kafka-python==2.0.2",
+      "kafka-python==2.0.*",
       "loguru==0.5.3",
       "psycopg2-binary==2.8.6",
       "pytest==6.1.2",
@@ -11,4 +11,7 @@ install_requires = [
 setup(name='lib',
       version='0.0.1',
       packages=find_packages('src'),
-      package_dir={"": "src"})
+      package_dir={"": "src"},
+      python_requires=">=3.6",
+      install_requires=install_requires
+      )
